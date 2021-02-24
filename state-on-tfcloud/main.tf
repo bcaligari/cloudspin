0eeab98198d959f411785dcb2fd15fed52fac6bd
Brendon Caligari <caligari@cypraea.co.uk>
2021-02-24 13:03:00 +0000

    state-on-cloud

--- state-on-tfcloud/main.tf
+++ state-on-tfcloud/main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.29"
     }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.46.0"
+    }
   }
 
   backend "remote" {
@@ -19,10 +24,32 @@ provider "aws" {
   region = "us-east-2"
 }
 
+provider "azurerm" {
+  features {
+  }
+}
+
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "tftest"
   }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-bc-tfcloudtest"
+  location = "westeurope"
+
+  tags     = {
+    owner = "Brendon Caligari"
+    status = "in use"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  name                = "vnet-default"
+  address_space       = ["172.16.0.0/16"]
 }
