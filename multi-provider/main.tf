4f5eda79ae7c37f55013a09fda7e5faac4b09025
Brendon Caligari <caligari@cypraea.co.uk>
2021-02-24 23:25:05 +0000

    multi-provider

--- multi-provider/main.tf
+++ multi-provider/main.tf
@@ -18,6 +18,12 @@ provider "azurerm" {
 }
 
 provider "aws" {
+  alias  = "e1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "e2"
   region = "us-east-2"
 }
 
@@ -52,7 +58,8 @@ resource "azurerm_subnet" "az_snet_oob" {
   address_prefixes     = ["172.16.1.0/24"]
 }
 
-resource "aws_vpc" "aws_vpc" {
+resource "aws_vpc" "e1_vpc" {
+  provider   = aws.e1
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -60,9 +67,29 @@ resource "aws_vpc" "aws_vpc" {
   }
 }
 
-resource "aws_subnet" "aws_snet" {
-  vpc_id     = aws_vpc.aws_vpc.id
+resource "aws_subnet" "e1_snet" {
+  provider   = aws.e1
+  vpc_id     = aws_vpc.e1_vpc.id
   cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "tftest-ooa"
+  }
+}
+
+resource "aws_vpc" "e2_vpc" {
+  provider   = aws.e2
+  cidr_block = "192.168.0.0/16"
+
+  tags = {
+    Name = "tftest"
+  }
+}
+
+resource "aws_subnet" "e2_snet" {
+  provider   = aws.e2
+  vpc_id     = aws_vpc.e2_vpc.id
+  cidr_block = "192.168.0.0/24"
 
   tags = {
     Name = "tftest-ooa"
